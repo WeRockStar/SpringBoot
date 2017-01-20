3dd740ee26a82a219d8bc36c667fc665b91bcbff
WeRockStar <kotchaphan.m@gmail.com>
2017-01-21 00:18:36 +0700

    Try to declare name value

--- src/main/java/com/werockstar/HomeController.java
+++ src/main/java/com/werockstar/HomeController.java
@@ -1,5 +1,6 @@
 package com.werockstar;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
+    @Value("${name}")
+    String name;
+
     @RequestMapping("/")
     @ResponseBody
     public String index() {
-        return "Hello World";
+        return name;
     }
 }
